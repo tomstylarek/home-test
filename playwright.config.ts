55a341802c44ceb23f7677819e6f47fb72c62948
Tomas Stylarek <tomasstylarek@gmail.com>
2025-07-15 17:36:16 -0300

    environment set up, installed dependencies, set up docker compose and playwright config to support multiple browsers and platforms

---
+++ playwright.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/tests',
+  timeout: 60 * 1000,
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: 0,
+  reporter: 'html',
+
+  use: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3100',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+
+  projects: [
+    /* Desktop */
+    {
+      name: 'chromium',
+      use: devices['Desktop Chrome'],
+    },
+    {
+      name: 'firefox',
+      use: devices['Desktop Firefox'],
+    },
+    {
+      name: 'webkit',
+      use: devices['Desktop Safari'],
+    },
+
+    /* Mobile */
+    {
+      name: 'Mobile Chrome',
+      use: devices['Pixel 5'],
+    },
+    {
+      name: 'Mobile Safari',
+      use: devices['iPhone 12'],
+    },
+  ],
+});
